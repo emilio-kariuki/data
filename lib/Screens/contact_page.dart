0389aafbe5c15351d1d6db6f75d181b8b73b8efb
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-05 01:35:17 +0300

    April 5, 2022 1:35 AM

--- lib/Screens/contact_page.dart
+++ lib/Screens/contact_page.dart
@@ -22,28 +22,30 @@ class Conntact extends StatelessWidget {
         ),
           title:  Text("Contact",style: GoogleFonts.redressed(fontSize: 28, color: Colors.white,fontWeight: FontWeight.w500))),
         backgroundColor: const Color.fromARGB(255, 7, 9, 15),
-        body: Column(
-          children: [
-            SizedBox(height: size.height * 0.03),
-            ContactUs(
-              logo: const AssetImage('assets/emilio.jpg'),
-              email: 'emilio113kariuki@gmail.com',
-              companyName: 'Emilio kariuki',
-              phoneNumber: '+254 796 250 443',
-              dividerThickness: 1,
-              companyFontSize: 30,
-              website: "https://linktr.ee/emiliokariuki",
-              githubUserName: 'emilio-kariuki',
-              linkedinURL: 'https://www.linkedin.com/in/emilio-kariuki/',
-              tagLine: 'Flutter Developer',
-              twitterHandle: 'HighTech113',
-              cardColor: Color.fromARGB(255, 158, 158, 158),
-              companyColor: Colors.white,
-              taglineColor: Colors.white,
-              textColor: Colors.white,
-            ),
-            Center(child: Text("Feel Free to reach out",style: GoogleFonts.redressed(fontSize: 28, color: Colors.white,fontWeight: FontWeight.w500)))
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: size.height * 0.03),
+              ContactUs(
+                logo: const AssetImage('assets/emilio.jpg'),
+                email: 'emilio113kariuki@gmail.com',
+                companyName: 'Emilio kariuki',
+                phoneNumber: '+254 796 250 443',
+                dividerThickness: 1,
+                companyFontSize: 30,
+                website: "https://linktr.ee/emiliokariuki",
+                githubUserName: 'emilio-kariuki',
+                linkedinURL: 'https://www.linkedin.com/in/emilio-kariuki/',
+                tagLine: 'Flutter Developer',
+                twitterHandle: 'HighTech113',
+                cardColor: Color.fromARGB(255, 158, 158, 158),
+                companyColor: Colors.white,
+                taglineColor: Colors.white,
+                textColor: Colors.white,
+              ),
+              Center(child: Text("Feel Free to reach out",style: GoogleFonts.redressed(fontSize: 28, color: Colors.white,fontWeight: FontWeight.w500)))
+            ],
+          ),
         ),
       ),
     );

6659b18b4eacda6ead95c2d91ca696291699e427
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-05 00:25:29 +0300

    April 5, 2022 12:25 AM

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -22,8 +22,10 @@ class _HomeState extends State<Home> {
   final district = TextEditingController();
   String? selectedDistrict;
   String? selectedGender;
+  String? selectedCommissioned;
   List<String> items = ["Centre", "Mugumo", "Makedonia", "pioneer"];
   List<String> itemGender = ["Male", "Female"];
+  List<String> itemCommissioned = ["Yes", "No"];
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -188,6 +190,74 @@ class _HomeState extends State<Home> {
             buttonHeight: size.height * 0.06,
             buttonWidth: size.width * 0.65,
             items: items
+                .map((item) => DropdownMenuItem<String>(
+                      value: item,
+                      child: Text(
+                        item,
+                        style: GoogleFonts.notoSerif(
+                            fontSize: 18,
+                            color: Color.fromARGB(255, 58, 57, 57),
+                            fontWeight: FontWeight.w600),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ))
+                .toList(),
+            buttonPadding: const EdgeInsets.only(left: 14, right: 14),
+            buttonDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                width: 1,
+                color: Color.fromARGB(255, 180, 182, 184),
+              ),
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+            buttonElevation: 5,
+            itemHeight: 35,
+            itemPadding: const EdgeInsets.only(left: 14, right: 14),
+            dropdownMaxHeight: 250,
+            dropdownWidth: 200,
+            dropdownPadding: EdgeInsets.only(top: 3),
+            dropdownDecoration: BoxDecoration(
+              color: Color.fromARGB(255, 238, 235, 235),
+            ),
+            dropdownElevation: 5,
+            scrollbarRadius: const Radius.circular(20),
+            scrollbarThickness: 10,
+            scrollbarAlwaysShow: true,
+            offset: const Offset(18, -50),
+          ),
+        ],
+      ),
+    );
+  }
+  Padding CommissionedDropdown(Size size) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, bottom: 8, top: 5),
+      child: Row(
+        children: [
+          LottieContain(lottieUrl: "assets/district_1.json"),
+          SizedBox(width: size.width * 0.06),
+          DropdownButton2(
+            hint: Text("Are you commissioned",
+                style:
+                    GoogleFonts.roboto(color: Color.fromARGB(255, 49, 49, 49))),
+            value: selectedCommissioned,
+            onChanged: (value) {
+              setState(() {
+                // getLocation();
+                selectedCommissioned = value as String;
+                print(selectedCommissioned);
+              });
+            },
+            icon: const Icon(
+              Icons.arrow_forward_ios_outlined,
+            ),
+            iconSize: 18,
+            iconEnabledColor: Colors.indigo,
+            iconDisabledColor: Color.fromARGB(255, 255, 255, 255),
+            buttonHeight: size.height * 0.06,
+            buttonWidth: size.width * 0.65,
+            items: itemCommissioned
                 .map((item) => DropdownMenuItem<String>(
                       value: item,
                       child: Text(

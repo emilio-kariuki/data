54b46ea9ac5c159fa47ae4c2ffe895e09f853fde
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-04 23:29:44 +0300

    April 4, 2022 11:29 PM

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names, avoid_print
 
 import 'package:data/build/lottie.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
@@ -18,6 +19,13 @@ class _HomeState extends State<Home> {
   final gender = TextEditingController();
   final name = TextEditingController();
   final district = TextEditingController();
+  String? selectedDistrict;
+  List<String> items = [
+    "Centre",
+    "Mugumo",
+    "Makedonia",
+    "pioneer"
+  ];
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -58,15 +66,14 @@ class _HomeState extends State<Home> {
                         height: 10,
                       ),
                     ),
-                    
-                    Itemz(size,"Name","assets/person_2.json",name),
-                    Itemz(size,"Gender","assets/gender_3.json",gender),
+                    Itemz(size, "Name", "assets/person_2.json", name),
+                    Itemz(size, "Gender", "assets/gender_3.json", gender),
                   ],
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left:8, right: 8),
+              padding: const EdgeInsets.only(left: 8, right: 8),
               child: Container(
                 height: size.height * 0.29,
                 width: size.width,
@@ -98,10 +105,79 @@ class _HomeState extends State<Home> {
                         height: 10,
                       ),
                     ),
-                    
-                    Itemz(size,"Phone Number","assets/phone_2.json",phone),
-                    Itemz(size,"Email","assets/mail_3.json",email),
-                    
+                    Itemz(size, "Phone Number", "assets/phone_2.json", phone),
+                    Itemz(size, "Email", "assets/mail_3.json", email),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 20, bottom: 8, top: 5),
+                      child: Row(
+                        children: [
+                          LottieContain(lottieUrl: "assets/lottie/select.json"),
+                          SizedBox(width: size.width * 0.06),
+                          DropdownButton2(
+                            hint: Text("Select",
+                                style: GoogleFonts.roboto(
+                                    color: Color.fromARGB(255, 49, 49, 49))),
+                            value: selectedDistrict,
+                            onChanged: (value) {
+                              setState(() {
+                                // getLocation();
+                                selectedDistrict = value as String;
+                                print(selectedDistrict);
+                              });
+                            },
+                            icon: const Icon(
+                              Icons.arrow_forward_ios_outlined,
+                            ),
+                            iconSize: 18,
+                            iconEnabledColor: Colors.indigo,
+                            iconDisabledColor:
+                                Color.fromARGB(255, 255, 255, 255),
+                            buttonHeight: size.height * 0.06,
+                            buttonWidth: size.width * 0.65,
+                            items: items
+                                .map((item) => DropdownMenuItem<String>(
+                                      value: item,
+                                      child: Text(
+                                        item,
+                                        style: GoogleFonts.notoSerif(
+                                            fontSize: 18,
+                                            color:
+                                                Color.fromARGB(255, 58, 57, 57),
+                                            fontWeight: FontWeight.w600),
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ))
+                                .toList(),
+                            buttonPadding:
+                                const EdgeInsets.only(left: 14, right: 14),
+                            buttonDecoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border: Border.all(
+                                width: 1,
+                                color: Color.fromARGB(255, 180, 182, 184),
+                              ),
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                            buttonElevation: 5,
+                            itemHeight: 35,
+                            itemPadding:
+                                const EdgeInsets.only(left: 14, right: 14),
+                            dropdownMaxHeight: 250,
+                            dropdownWidth: 200,
+                            dropdownPadding: EdgeInsets.only(top: 3),
+                            dropdownDecoration: BoxDecoration(
+                              color: Color.fromARGB(255, 238, 235, 235),
+                            ),
+                            dropdownElevation: 5,
+                            scrollbarRadius: const Radius.circular(20),
+                            scrollbarThickness: 10,
+                            scrollbarAlwaysShow: true,
+                            offset: const Offset(18, -50),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -112,47 +188,43 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Padding Itemz(Size size,String name,String lottieUrl,TextEditingController action) {
+  Padding Itemz(
+      Size size, String name, String lottieUrl, TextEditingController action) {
     return Padding(
-                          padding: const EdgeInsets.only(
-                              left: 20, bottom: 8, top: 5),
-                          child: Row(
-                            children: [
-                               LottieContain(
-                                  lottieUrl: lottieUrl,),
-                              SizedBox(width: size.width * 0.06),
-                              Flexible(
-                                flex: 1,
-                                child: TextFormField(
-                                  //keyboardType: TextInputType.multiline,
-                                  // style: TextStyle(height:10),
+      padding: const EdgeInsets.only(left: 20, bottom: 8, top: 5),
+      child: Row(
+        children: [
+          LottieContain(
+            lottieUrl: lottieUrl,
+          ),
+          SizedBox(width: size.width * 0.06),
+          Flexible(
+            flex: 1,
+            child: TextFormField(
+              //keyboardType: TextInputType.multiline,
+              // style: TextStyle(height:10),
 
-                                  decoration: InputDecoration(
-                                      isDense: true,
-                                      contentPadding:
-                                          EdgeInsets.fromLTRB(15, 20, 15, 15),
-                                      // contentPadding: const EdgeInsets.symmetric(vertical: 40.0),
-                                      border: OutlineInputBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(10.0),
-                                      ),
-                                      // icon: Icon(Icons.person,size:30,color: Colors.black),
-                                      filled: true,
-                                      hintStyle:
-                                          TextStyle(color: Colors.grey[800]),
-                                      focusColor: Colors.red,
-                                      hintText: name,
-                                      // suffixText: "10",
-                                      // prefixIcon: Icon(Icons.mail,color: Colors.blueGrey[900]),
-                                      fillColor: Colors.grey[200]),
-                                  controller: action,
-                                ),
-                              ),
-                              SizedBox(width: 15),
-                            ],
-                          ),
-                        );
+              decoration: InputDecoration(
+                  isDense: true,
+                  contentPadding: EdgeInsets.fromLTRB(15, 20, 15, 15),
+                  // contentPadding: const EdgeInsets.symmetric(vertical: 40.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  // icon: Icon(Icons.person,size:30,color: Colors.black),
+                  filled: true,
+                  hintStyle: TextStyle(color: Colors.grey[800]),
+                  focusColor: Colors.red,
+                  hintText: name,
+                  // suffixText: "10",
+                  // prefixIcon: Icon(Icons.mail,color: Colors.blueGrey[900]),
+                  fillColor: Colors.grey[200]),
+              controller: action,
+            ),
+          ),
+          SizedBox(width: 15),
+        ],
+      ),
+    );
   }
-
-  
 }

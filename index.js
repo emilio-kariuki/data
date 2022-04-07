7d41a791f196214b3471696ec4ef59bf649bf431
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 13:32:49 +0300

    April 7, 2022 1:32 PM

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ mongoose.Promise = global.Promise;
 app.use(express.json());
 app.use('/api', require('./routes/api'));
 // app.get('/api',(req,res) => res.send("Api working fine"));
-app.use(function(req,res,next,err){
-    res.status(422).send({error: err.message});
-});
+// app.use(function(req,res,next,err){
+//     res.status(422).send({error: err.message});
+// });
 
 
 app.listen(process.env.port || 8080, function(){

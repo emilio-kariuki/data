7d2aefa584c7122a21e026d6fa8d222e099f19fd
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 17:11:22 +0300

    April 7, 2022 5:11 PM

--- database/database.js
+++ database/database.js
@@ -3,15 +3,19 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-mongodb.connect(
+export const mongoDbConnection = mongodb.connect(
     process.env.port.MONGODB_URL,
     {
         useUnifiedTopology: true,
     },
     (error,client)=>{
         if(error) throw error;
+        console.log
         const database = client.db(process.env.MONGODB_URL);
         const collection = database.collection("data");
-        await collection.find().toArray();;
+        // await collection.find().toArray();
+
+        var dataFromDb = await collection.find().toArray();
+        console.log(dataFromDb)
     }
 );

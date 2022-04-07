124c92b6ea2107cb9483ebaac37a018f23c00e78
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 16:00:59 +0300

    April 7, 2022 4:00 PM

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('',(res,req)=> res.json({name: "Emilio"}));
+app.use(express.json());
+app.get('/',(res,req)=> res.json({name: "Emilio"}));
 app.listen(3000,()=> console.log("Server running"));

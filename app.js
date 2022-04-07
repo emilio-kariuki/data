50dc0dadca71a0049b066a7100b810727705ed53
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 17:24:58 +0300

    April 7, 2022 5:24 PM

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import mongoDbConnection from '../database/database';
-import express, { urlencoded, json } from 'express';
-import router from './routes/routes';
+const mongoDbConnection = require('./database/database');
+const express = require('express');
+const router = require('./routes/routes');
 const app = express();
 
-app.use(urlencoded({ extended: false}));
-app.use(json());
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 mongoDbConnection();
 app.use('/', router);
 app.listen(8080, () => console.log("Server rocking at port 8080"));

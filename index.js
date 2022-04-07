c992ad030f14a74ab09907c4d16b187747f499a4
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 12:53:18 +0300

    April 7, 2022 12:53 PM

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/api',(req,res) => res.send("Api working fine"));
+app.get('',(req,res) => res.send("Api working fine"));
 
 
 app.listen(process.env.port || 8080, function(){

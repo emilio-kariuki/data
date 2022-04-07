165c21a404d86821788d986a148b9fc9f41fadd1
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 16:19:49 +0300

    April 7, 2022 4:19 PM

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-app.get('/',(req,res)=> {res.status(200).json({name: "Emilio"})});
-app.listen(3000,()=> console.log("Server running"));
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.get('/', (req, res) => {
+  res.status(200).json({ name: 'Emilio' })
+});
+app.post('/add',(req, res)=>{
+    const data = (num_1,num_2);
+    res.send("The sum is " + num_1 + num_2);
+    res.end()
+
+})
+app.listen(3000, () => console.log('Server running'))

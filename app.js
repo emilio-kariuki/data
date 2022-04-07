6b4cca6a19b9a1834af542912dde93f70b08bd4b
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 17:18:06 +0300

    April 7, 2022 5:18 PM

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const mongoDbConnection = require('../database/database');
 const express = require('express');
-const app = express()
+const app = express();
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+mongoDbConnection();

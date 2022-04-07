1084ac43fcccce5ba6f7a0b07014548841c335b6
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 16:29:47 +0300

    April 7, 2022 4:29 PM

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.post('/add',(req, res)=>{
     const {name,email} = req.body;
     console.log(name);
     console.log(email);
-    res.json({name: {
+    res.json({
         name,email
-    }});
+    });
     res.end()
 
 })

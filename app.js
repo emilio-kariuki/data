6a2a2811b9d69a6b96e04612566224828bcf3cbf
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 17:19:51 +0300

    April 7, 2022 5:19 PM

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const mongoDbConnection = require('../database/database');
 const express = require('express');
+const router = require('./routes/routes');
 const app = express();
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 mongoDbConnection();
+app.use('/', router);
+app.listen(8080, () => console.log("Server rocking at port 8080"));

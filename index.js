da7769cced2cd6656b606eb8611b2790fd4f1343
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 15:33:01 +0300

    April 7, 2022 3:33 PM

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+const app =  express();
 
-mongoose.connect('mongodb://localhost/data');
-mongoose.Promise = global.Promise;
-app.use(express.json());
-app.use('/api', require('./routes/api'));
-// app.get('/api',(req,res) => res.send("Api working fine"));
-// app.use(function(req,res,next,err){
-//     res.status(422).send({error: err.message});
-// });
-
-
-app.listen(process.env.port || 8080, function(){
-    console.log("Server started");
-}
-
-);
+app.listen(process.env.port || 8080, () => console.log("Server Started"));

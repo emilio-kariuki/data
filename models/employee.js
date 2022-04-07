d8edfb4a7e1fd1dab578d517625a661b54f49a3f
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 13:39:06 +0300

    April 7, 2022 1:39 PM

--- models/employee.js
+++ models/employee.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const EmployeeSchema = new schema({
+const EmployeeSchema = new Schema({
     name: {
         type: String
     },

83a39e0e95fb7a7b73843dce9e2499d01c02f1ad
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 13:23:57 +0300

    April 7, 2022 1:23 PM

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Employee = require('../models/employee');
 
 router.get('/employee',(req,res)=>res.send({type: "Get"})
 );

7215093e39221ce91a1177e391dab257a22d9add
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 15:34:24 +0300

    April 7, 2022 3:34 PM

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app =  express();
-
+app.get('',(req, res)=>
+    res.send("Emilio started the server")
+);
 app.listen(process.env.port || 8080, () => console.log("Server Started"));

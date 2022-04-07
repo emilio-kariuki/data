4097abf3886ed719402c0e903c0ce22bfd20071b
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-07 15:59:33 +0300

    April 7, 2022 3:59 PM

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const app =  express();
-app.get('',(req, res)=>
-    res.send("Emilio started the server in Dehub")
-);
-app.listen(process.env.port || 8080, () => console.log("Server Started"));
+const app = express();
+app.get('',(res,req)=> res.json({name: "Emilio"}));
+app.listen(3000,()=> console.log("Server running"));

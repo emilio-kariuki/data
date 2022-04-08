63d274f86fa0df25410af2f8b6b6b396d78a86fd
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-09 01:36:24 +0300

    April 9, 2022 1:36 AM

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const app = express();
-const User = require('../models/index.model');
+const User = require("../models/index.model");
 
-app.post('/register', (req, res)=>{
-    console.log("Inside the register");
-    const user = new User({
-      name: req.body.name,
-      password: req.body.password,
-      email:req.body.email
-    });
-    user.save().then(() => {
-     console.log("User has been saved");
-     res.status(200).json("ok").catch(err => {
-       res.status(403).json(err);
-       
-     })
-    })
-  })
+app.post("/register", (req, res) => {
+  console.log("Inside the register");
+  const user = new User({
+    name: req.body.name,
+    password: req.body.password,
+    email: req.body.email,
+  });
+  user.save().then(() => {
+    console.log("User has been saved");
+    res
+      .status(200)
+      .json("ok")
+      .catch((err) => {
+        res.status(403).json(err);
+      });
+  });
+});
 
-  module.exports = router;
+module.exports = router;

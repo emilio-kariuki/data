4dbc3838ae71bec350681d97e8e7ca0ecf26a55c
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-08 11:12:14 +0300

    April 8, 2022 11:12 AM

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import { MongoClient } from 'mongodb';
 
 async function main(){
-  const uri = process.env.MONGODB_URL;
+  const uri = 'mongodb+srv://emilio:0722937438@cluster0.xdlrq.mongodb.net/myFirstDatabase?retryWrites=true&w=majorityL';
   const client = new MongoClient(uri);
   try{
     await client.connect();
+    console.log("Conneceted to database");
   }catch(e){
     console.error(e);
   }finally{

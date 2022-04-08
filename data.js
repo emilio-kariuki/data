cc7de5767db8a7d7bfd7f6ab55de8cc7b5c73c0d
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-08 13:10:03 +0300

    April 8, 2022 1:10 PM

--- data.js
+++ data.js
@@ -1,47 +1,47 @@
-import { MongoClient } from "mongodb";
+// import { MongoClient } from "mongodb";
 
-const uri =
-    "mongodb+srv://emilio:0722937438@cluster0.xdlrq.mongodb.net/myFirstDatabase?retryWrites=true";
-  const client = new MongoClient(uri);
+// const uri =
+//     "mongodb+srv://emilio:0722937438@cluster0.xdlrq.mongodb.net/myFirstDatabase?retryWrites=true";
+//   const client = new MongoClient(uri);
 
-export async function main() {
+// export async function main() {
   
-  try {
-    await client.connect();
-    console.log("Conneceted to database");
-    // await listDatabase(client);
-    // 
-    await findListingWithName(client, "emilio");
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close();
-  }
-}
-main().catch(console.error);
+//   try {
+//     await client.connect();
+//     console.log("Conneceted to database");
+//     // await listDatabase(client);
+//     // 
+//     await findListingWithName(client, "emilio");
+//   } catch (e) {
+//     console.error(e);
+//   } finally {
+//     await client.close();
+//   }
+// }
+// main().catch(console.error);
 
-async function listDatabase(client) {
-  const databasesList = await client.db().admin().listDatabases();
-  console.log("Databases:");
-  databasesList.databases.forEach((db) => console.log(" -" + db.name));
-}
+// async function listDatabase(client) {
+//   const databasesList = await client.db().admin().listDatabases();
+//   console.log("Databases:");
+//   databasesList.databases.forEach((db) => console.log(" -" + db.name));
+// }
 
-async function createListing(client, newListing) {
-  const result = await client
-    .db("youthing")
-    .collection("data")
-    .insertOne(newListing);
-  console.log("New Listing added with the following id : " + result.insertedId);
-}
-async function findListingWithName(client, newListingName){
-  const result = await client.db('youth').collection('data').findOne({name: newListingName});
-  if(result){
-    console.log("The result found is " + result);
-    console.log(result);
-  }else{
-    console.log("No results found in the database");
-  }
-}
+// async function createListing(client, newListing) {
+//   const result = await client
+//     .db("youthing")
+//     .collection("data")
+//     .insertOne(newListing);
+//   console.log("New Listing added with the following id : " + result.insertedId);
+// }
+// async function findListingWithName(client, newListingName){
+//   const result = await client.db('youth').collection('data').findOne({name: newListingName});
+//   if(result){
+//     console.log("The result found is " + result);
+//     console.log(result);
+//   }else{
+//     console.log("No results found in the database");
+//   }
+// }
 
 
 
--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-import { MongoClient } from "mongodb";
-import express from 'express';
-import {main} from './main';
+const mongoose = require("mongoose");
+const express = require("express");
 const app = express();
 app.use(express.json());
 
-
-  app.get('/', async function (req, res) {
-    res.send(main.findListingWithName(client, "emilio"));
-  })
-
-app.listen(3000, () => console.log("Server started and is running"));
-
+app.listen

2fb1df9849d7f660e5e66ebcbcdfbea3a2a36aa4
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-08 13:18:36 +0300

    April 8, 2022 1:18 PM

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.use(express.json());
 
 const port = process.env.port || 5000;
 
+app.get('/', (req, res) => {
+  res.send("Response has been sent");
+})
+
 app.listen(port,() => console.log("Server connect at " + port));

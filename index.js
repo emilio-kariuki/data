581f2549193685a9bdbd8c22d14a4729d692d653
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-08 11:38:50 +0300

    April 8, 2022 11:38 AM

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
-async function main(){
-  const uri = 'mongodb+srv://emilio:0722937438@cluster0.xdlrq.mongodb.net/myFirstDatabase?retryWrites=true&w=majorityL';
+async function main() {
+  const uri =
+    "mongodb+srv://emilio:0722937438@cluster0.xdlrq.mongodb.net/myFirstDatabase?retryWrites=true&w=majorityL";
   const client = new MongoClient(uri);
-  try{
+  try {
     await client.connect();
     console.log("Conneceted to database");
     // await listDatabase(client);
-    await createListing(client,
-       {
-         name: "Jane",});
-  }catch(e){
+    await createListing(client, {
+      name: "Jane",
+      email: "Joy113karis@gmail.com",
+    });
+  } catch (e) {
     console.error(e);
-  }finally{
+  } finally {
     await client.close();
   }
- 
 }
 main().catch(console.error);
 
-async function listDatabase(client){
+async function listDatabase(client) {
   const databasesList = await client.db().admin().listDatabases();
-  console.log('Databases:');
-  databasesList.databases.forEach(db => console.log(' -' + db.name));
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(" -" + db.name));
 }
 
-async function createListing(client, newListing){
-  const result = await client.db('youth').collection("data").insertOne(newListing);
-  console.log('New Listing added with the following id : ' + result.insertedId);
-
+async function createListing(client, newListing) {
+  const result = await client
+    .db("youth")
+    .collection("data")
+    .insertOne(newListing);
+  console.log("New Listing added with the following id : " + result.insertedId);
 }

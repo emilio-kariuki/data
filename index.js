91d144f17398ffbd401ce944f153a353a08c2011
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-08 13:14:34 +0300

    April 8, 2022 1:14 PM

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.listen
+const port = process.env.port || 5000;
+
+app.listen(port,() => console.log("Server connect at " + port));

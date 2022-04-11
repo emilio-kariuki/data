0ef1790152b9415f837ff5ca22b79243dd3e7e8a
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-11 16:46:30 +0300

    April 11, 2022 4:46 PM

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -78,7 +78,7 @@ class _HomeState extends State<Home> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
-                  height: size.height * 0.21,
+                  height: size.height * 0.23,
                   width: size.width,
                   padding: const EdgeInsets.all(3),
                   decoration: BoxDecoration(

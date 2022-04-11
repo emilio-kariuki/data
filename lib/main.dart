2494cba5520ef297c3728ecfe1d870e637224560
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-12 01:01:46 +0300

    April 12, 2022 1:01 AM

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'Screens/Home.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp( );
   runApp(const GetMaterialApp(
     home: Home(),
     debugShowCheckedModeBanner: false,

9d03aedbd9b7600a531e33e9196cd0860be28a58
Emilio kariuki <emilio113kariuki@gmail.com>
2022-04-12 02:09:26 +0300

    April 12, 2022 2:09 AM

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -849,74 +849,7 @@ class _HomeState extends State<Home> {
     );
   }
 
-  // Padding GenderDropdown(Size size) {
-  //   return Padding(
-  //     padding: const EdgeInsets.only(left: 20, bottom: 8, top: 5),
-  //     child: Row(
-  //       children: [
-  //         LottieContain(lottieUrl: "assets/gender_3.json"),
-  //         SizedBox(width: size.width * 0.06),
-  //         DropdownButton2(
-  //           hint: Text("Gender",
-  //               style:
-  //                   GoogleFonts.roboto(color: Color.fromARGB(255, 49, 49, 49))),
-  //           value: selectedGender,
-  //           onChanged: (value) {
-  //             setState(() {
-  //               // getLocation();
-  //               selectedGender = value as String;
-  //               print(selectedGender);
-  //             });
-  //           },
-  //           icon: const Icon(
-  //             Icons.arrow_forward_ios_outlined,
-  //           ),
-  //           iconSize: 18,
-  //           iconEnabledColor: Colors.indigo,
-  //           iconDisabledColor: Color.fromARGB(255, 255, 255, 255),
-  //           buttonHeight: size.height * 0.06,
-  //           buttonWidth: size.width * 0.65,
-  //           items: itemGender
-  //               .map((item) => DropdownMenuItem<String>(
-  //                     value: item,
-  //                     child: Text(
-  //                       item,
-  //                       style: GoogleFonts.notoSerif(
-  //                           fontSize: 18,
-  //                           color: Color.fromARGB(255, 58, 57, 57),
-  //                           fontWeight: FontWeight.w600),
-  //                       overflow: TextOverflow.ellipsis,
-  //                     ),
-  //                   ))
-  //               .toList(),
-  //           buttonPadding: const EdgeInsets.only(left: 14, right: 14),
-  //           buttonDecoration: BoxDecoration(
-  //             borderRadius: BorderRadius.circular(10),
-  //             border: Border.all(
-  //               width: 1,
-  //               color: Color.fromARGB(255, 180, 182, 184),
-  //             ),
-  //             color: Color.fromARGB(255, 255, 255, 255),
-  //           ),
-  //           buttonElevation: 5,
-  //           itemHeight: 35,
-  //           itemPadding: const EdgeInsets.only(left: 14, right: 14),
-  //           dropdownMaxHeight: 250,
-  //           dropdownWidth: 200,
-  //           dropdownPadding: EdgeInsets.only(top: 3),
-  //           dropdownDecoration: BoxDecoration(
-  //             color: Color.fromARGB(255, 238, 235, 235),
-  //           ),
-  //           dropdownElevation: 5,
-  //           scrollbarRadius: const Radius.circular(20),
-  //           scrollbarThickness: 10,
-  //           scrollbarAlwaysShow: true,
-  //           offset: const Offset(18, -50),
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
+  
 
   Padding Itemz(Size size, String name, String lottieUrl,
       TextEditingController action, TextInputType type) {
